24a0255147f590e9dabbfbbade7c9ca475c122d3
louis de Cheffontaines <louis2cheff@hotmail.fr>
2021-05-14 12:19:01 +0200

    add bundle lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#  resources :movies, only: [:new, :create, :index, :destroy, :show, :edit, :update, :destroy] do
+
+resources :lists, only: [:new, :create, :index, :show] do
+  resources :bookmarks, only: [:edit, :update, :show, :destory]
 end
+end
+
